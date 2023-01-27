be63548494a52e4f40248d5cbb9ff1437bfc91b6
Salomon Chambi <salomonchambi@gmail.com>
2023-01-27 01:28:30 -0500

    feat: use from_object() method to set the app config

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,12 @@
 ''' Create a Flask server '''
 from flask import Flask
 
-app = Flask(__name__)
 
-
-def create_app():
+def create_app(environment):
     ''' Create a Flask server '''
+    app = Flask(__name__)
+
+    # Use 'environment' object to set the app configuration
+    app.config.from_object(environment)
+
     return app
