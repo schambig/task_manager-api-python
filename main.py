96f6192690b4cceccf7bd1df42316642b42d7e08
Salomon Chambi <salomonchambi@gmail.com>
2023-03-03 12:05:00 -0500

    feat: add guard to the main script

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from config import config
 environment = config['development']
 app = create_app(environment)
 
-
+# Check whether the current script is being run as the main program
+# or if it is being imported as a module by another script
 if __name__ == '__main__':
     app.run()
