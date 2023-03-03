81249ae997f137887c70b2344224300d7fa9a080
Salomon Chambi <salomonchambi@gmail.com>
2023-03-03 12:06:00 -0500

    feat: add descriptive comment

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from flask import Flask
 
 def create_app(environment):
     ''' Create a Flask server '''
+    # Creates a new instance of a Flask web application,
+    # and use special variable __name__, which represents the name of the current module
     app = Flask(__name__)
 
     # Use 'environment' object to set the app configuration
