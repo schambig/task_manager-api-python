c78eedd137deceafc65ae6f17633f9967b48f086
Salomon Chambi <salomonchambi@gmail.com>
2023-03-04 12:09:00 -0500

    feat: add database path and don't track mods to database

--- config.py
+++ config.py
@@ -9,6 +9,8 @@ class Config:
 class DevelopmentConfig(Config):
     ''' Development environment configuration class '''
     DEBUG = True
+    SQLALCHEMY_DATABASE_SALO = 'postgresql://localhost/api'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Dictionary to store class Development, which will store configurations for the app
 config = {
