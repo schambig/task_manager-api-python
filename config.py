cd1e4dcd37b9cccf4768e55bc5dff67576a30356
Salomon Chambi <salomonchambi@gmail.com>
2023-03-04 12:12:00 -0500

    fix: correct the database path variable name

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
 class DevelopmentConfig(Config):
     ''' Development environment configuration class '''
     DEBUG = True
-    SQLALCHEMY_DATABASE_SALO = 'postgresql://localhost/api'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/api'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Dictionary to store class Development, which will store configurations for the app
