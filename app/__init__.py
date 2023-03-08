c66f86437d1b6e734e5ea6f45756fb8012838a1b
Salomon Chambi <salomonchambi@gmail.com>
2023-03-08 12:16:00 -0500

    feat: create tasks table in database

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,21 @@
 ''' Create a Flask server '''
 from flask import Flask
 from .models import db
+from .models.task import Task
 
+# Creates a new instance of a Flask web application,
+# and use special variable __name__, which represents the name of the current module
+app = Flask(__name__)
 
 def create_app(environment):
     ''' Create a Flask server '''
-    # Creates a new instance of a Flask web application,
-    # and use special variable __name__, which represents the name of the current module
-    app = Flask(__name__)
-
     # Use 'environment' module that contains the configuration settings for the application.
     app.config.from_object(environment)
+
+
+    # Create tasks table in DB
+    with app.app_context():
+        db.init_app(app)
+        db.create_all()
 
     return app
