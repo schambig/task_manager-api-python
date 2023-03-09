49bd08252670dd75da161e3790fc750792cf0782
Salomon Chambi <salomonchambi@gmail.com>
2023-03-09 12:17:00 -0500

    feat: register all routes from views module

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from .models import db
 from .models.task import Task
+from .views import api_v1
 
 # Creates a new instance of a Flask web application,
 # and use special variable __name__, which represents the name of the current module
@@ -13,6 +14,8 @@ def create_app(environment):
     # Use 'environment' module that contains the configuration settings for the application.
     app.config.from_object(environment)
 
+    # Register all routes from views module
+    app.register_blueprint(api_v1)
 
     # Create tasks table in DB
     with app.app_context():
