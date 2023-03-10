cdf9938337977e861909a1fa5f80f586afbc3dfe
Salomon Chambi <salomonchambi@gmail.com>
2023-03-10 12:23:00 -0500

    feat: add tasks when table is created using listen event

--- app/models/task.py
+++ app/models/task.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 ''' Task model to work with the api '''
 from . import db
+from sqlalchemy.event import listen # To trigger actions when a certain event happens
 
 # Use a class (db.Model) provided by SQLAlchemy's ORM (Object-Relational Mapping) library
 class Task(db.Model):
@@ -17,3 +18,17 @@ class Task(db.Model):
     # Return title attribute when __str__() method is called
     def __str__(self):
         return self.title
+
+# Pass a variable number of positional and keyword arguments to the function
+# those arguments are collected into a tuple and dictionary respectively
+def insert_tasks(*args, **kwargs):
+    db.session.add(
+        Task(title='Title 1', description='Description', deadline='2023-03-10 12:00:00')
+    )
+    db.session.add(
+        Task(title='Title 2', description='Description', deadline='2023-03-10 12:00:00')
+    )
+    # Persist the previous tasks
+    db.session.commit()
+
+listen(Task.__table__, 'after_create', insert_tasks)
